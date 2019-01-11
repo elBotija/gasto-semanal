635f70a2651908c159f555a8ab064395f9c02588
mmartinez <mmartinez@dosalcubo.com>
2019-01-11 15:06:55 -0300

    Updates

---
+++ precache-manifest.78c18ca892e2c9afd158faed72fd8b14.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1389ad3a3ca56f00b23b",
+    "url": "/gasto-semanal/static/js/runtime~main.1389ad3a.js"
+  },
+  {
+    "revision": "9f97d122fc604ec8d2cd",
+    "url": "/gasto-semanal/static/js/main.9f97d122.chunk.js"
+  },
+  {
+    "revision": "993ee873b8d93dd276b6",
+    "url": "/gasto-semanal/static/js/1.993ee873.chunk.js"
+  },
+  {
+    "revision": "9f97d122fc604ec8d2cd",
+    "url": "/gasto-semanal/static/css/main.5e81db6b.chunk.css"
+  },
+  {
+    "revision": "993ee873b8d93dd276b6",
+    "url": "/gasto-semanal/static/css/1.243bed18.chunk.css"
+  },
+  {
+    "revision": "f9a8f67a87876ed04ffddbd41499222a",
+    "url": "/gasto-semanal/index.html"
+  }
+];
